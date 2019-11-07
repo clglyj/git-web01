bc1d772dd17000df377ee7ae1d9e8807347fa48b
clglyj@126 <1401309131@qq.com>
2019-11-07 09:38:11 +0800

    解决冲突

--- src/main/java/com/atguigu/MyTest.java
+++ src/main/java/com/atguigu/MyTest.java
@@ -3,5 +3,7 @@ package com.atguigu;
 public class MyTest {
 
 	public static void main(String[] args) {
+		System.out.println("======================");
+		System.out.println("======================");
 	}
 }
