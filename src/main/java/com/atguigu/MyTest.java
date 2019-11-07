0d1c649f7203e137f35e9d96343c973964d9e608
clglyj@126 <1401309131@qq.com>
2019-11-07 09:31:44 +0800

    jjjj

--- src/main/java/com/atguigu/MyTest.java
+++ src/main/java/com/atguigu/MyTest.java
@@ -3,22 +3,5 @@ package com.atguigu;
 public class MyTest {
 
 	public static void main(String[] args) {
-		System.out.println("======================");
-		System.out.println("======================");
-		System.out.println("======================");
-		System.out.println("======================");
-		System.out.println("======================");
-		System.out.println("======================");
-		System.out.println("======================");
-		System.out.println("======================");
-		System.out.println("======================");
-		System.out.println("mastermastermastermastermastermastermastermastermastermastermastermaster");
-		System.out.println("mastermastermastermastermastermastermastermastermastermastermastermaster");
-		System.out.println("---------");
-		System.out.println("devdevdevdevdevdevdevdevdevdevdevdev");
-		System.out.println("devdevdevdevdevdevdevdevdevdevdevdev");
-		System.out.println("devdevdevdevdevdevdevdevdevdevdevdev");
-		System.out.println("master修改");
-		System.out.println("-----------------------master修改");
 	}
 }
